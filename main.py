c7c0f963683b22fdc069791c919cb13c59ac8817
hardenyu21 <hardenyu221@gmail.com>
2024-12-09 15:41:35 +0800

    .

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     """
 
     from src.pipeline.stablediffusion import WaterMarkedStableDiffusionPipeline
+    from diffusers import StableDiffusionPipeline
     import torch
     torch.manual_seed(42)
     import warnings
@@ -33,7 +34,12 @@ if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "cpu"
     w_pipe = WaterMarkedStableDiffusionPipeline("CompVis/stable-diffusion-v1-4")
     w_pipe.to(device)
-    images, msgs = w_pipe.generate(['A man is walking on the road', 'A man is playing basketball'], return_msg = True)
-    for i, img in enumerate(images):
-        img.save(f"test{i+2}.png")
-        print(msgs[i])
+    w_images, msgs = w_pipe.generate(['A man is walking on the road'], return_msg = True)
+    non_w_pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
+    non_w_pipe.to(device)
+    non_w_images = non_w_pipe(['A man is walking on the road'])[0]
+    
+    for w_image in w_images:
+        w_image.save("w_image.png")
+    for non_w_image in non_w_images: 
+        non_w_image.save("non_w_image.png")
